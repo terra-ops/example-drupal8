bfa6c3e075e734472644c9ff9ca3c961028aff42
Jon Pugh <jon@thinkdrop.net>
2015-07-11 12:30:06 -0400

    Adding apps output to Drupal!

--- modules/terra/terra_server/src/Controller/DefaultController.php
+++ modules/terra/terra_server/src/Controller/DefaultController.php
@@ -8,6 +8,8 @@
 namespace Drupal\terra_server\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\Yaml\Parser;
+use Drupal\Component\Utility\SafeMarkup;
 
 /**
  * Class DefaultController.
@@ -25,12 +27,41 @@ class DefaultController extends ControllerBase {
 
     //Immutable Config (Read Only)
     $config = \Drupal::config('terra.settings');
-    $path = 'path:' . $config->get('path_to_config');
+    $path = $config->get('path_to_config');
 
-    return [
+    $yaml = new Parser();
+    $terra_config = $yaml->parse(file_get_contents($path));
+
+    $output = array();
+    $output['note'] = [
       '#type' => 'markup',
-      '#markup' => $this->t('Hello Terra!') . $path
+      '#markup' => $this->t('Hello, Terra!'),
     ];
+    $output['apps'] = [
+      '#type' => 'table',
+      '#header' => array(t('Name'), t('Description'), t('Repo'), t('Environments')),
+      '#empty' => t('There are no apps yet.'),
+    ];
+
+    foreach ($terra_config['apps'] as $name => $app) {
+      $output['apps'][$name]['title'] = array(
+        '#markup' => SafeMarkup::checkPlain($app['name']),
+      );
+      $output['apps'][$name]['description'] = array(
+        '#markup' => SafeMarkup::checkPlain($app['description']),
+      );
+      $output['apps'][$name]['repo'] = array(
+        '#markup' => SafeMarkup::checkPlain($app['repo']),
+      );
+
+      $environments = is_array($app['environments']) ? implode(', ', array_keys($app['environments'])) : 'None';
+      $output['apps'][$name]['environments'] = array(
+        '#markup' => SafeMarkup::checkPlain($environments),
+      );
+
+    }
+
+    return $output;
   }
 
 }
